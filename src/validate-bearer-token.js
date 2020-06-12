5a26a029be995e62d58cc387a246766c3843938c
Andrew Durrant <andrew.durrant75@gmail.com>
2020-06-12 16:17:44 -0600

    fixed auth and endpoint issues

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,7 +3,8 @@ const { API_TOKEN } = require('./config');
 function validateBearerToken(req, res, next) {
   const apiToken = API_TOKEN;
   const authToken = req.get('Authorization');
-
+  console.log('HEADS UP', apiToken, authToken);
+  
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
